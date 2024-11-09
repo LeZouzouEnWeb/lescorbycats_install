1bcb06539220db6d15c66076386b31c3800468f6
ecorbisiersimplon <ecorbisier.simplon@gmail.com>
2024-11-09 14:34:25 +0100

    Eric//Ajout de la prise en charge du lancement de docker desktop avant de lancer les serveurs

--- play_symf.sh
+++ play_symf.sh
@@ -4,6 +4,44 @@ clear
 
 layout="$PWD/install"
 source "$layout/variables.sh"
+docker-desktop() {
+    /opt/docker-desktop/bin/docker-desktop
+}
+
+if ! docker images >/dev/null 2>&1; then
+    echo "Lancement de Docker desktop"
+    echo "--------------------------------"
+    pause s 1 m
+    docker-desktop
+    echo " ** Lancement en cours ... **"
+
+    i=0
+    while true; do
+        ((i++))
+        printf "\r"
+        printf "\033[J"
+        printf "Appuyez sur [\033[36;5mq\033[0m] pour quitter (%ds) > " "$i"
+
+        read -t 1 -n 1 -s keys # Lire un seul caractère en mode silencieux
+
+        if [[ "${keys^^}" == "Q" ]]; then
+            printf "\n"
+            printf "\r"
+            printf "\033[J"
+            echo "Attente annulée"
+            exit 1
+        fi
+
+        if docker images >/dev/null 2>&1; then
+            printf "\n"
+            printf "\r"
+            printf "\033[J"
+            echo "Lancement effectué"
+            break
+        fi
+    done
+
+fi
 
 if [ -d "$folder_rel_serveur" ]; then
     cd $folder_rel_serveur
